93e8dd97f0492c586e0306450a95c508a8aca6fe
Nat! <nat@mulle-kybernetik.com>
2020-04-01 21:44:10 +0200

    fix missing env variables for travis

--- test/.mulle/etc/env/environment-host-travis-ci-prerelease.sh
+++ test/.mulle/etc/env/environment-host-travis-ci-prerelease.sh
@@ -17,3 +17,39 @@ export MULLE_TESTALLOCATOR_URL="https://github.com/mulle-core/mulle-testallocato
 export MULLE_TESTALLOCATOR_BRANCH="prerelease"
 
 
+#
+# MulleObjCStandardFoundation-startup nodetype: ${MULLE_OBJC_STANDARD_FOUNDATION_STARTUP_NODETYPE:-tar}
+#
+export MULLE_OBJC_STANDARD_FOUNDATION_STARTUP_NODETYPE="git"
+
+
+#
+# MulleObjCStandardFoundation-startup url: ${MULLE_OBJC_STANDARD_FOUNDATION_STARTUP_URL:-https://github.com/MulleFoundation/MulleObjCStandardFoundation-startup/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_OBJC_STANDARD_FOUNDATION_STARTUP_URL="https://github.com/MulleFoundation/MulleObjCStandardFoundation-startup.git"
+
+
+#
+# MulleObjCStandardFoundation-startup branch: ${MULLE_OBJC_STANDARD_FOUNDATION_STARTUP_BRANCH}
+#
+export MULLE_OBJC_STANDARD_FOUNDATION_STARTUP_BRANCH="prerelease"
+
+
+#
+# MulleObjCHTTPFoundation nodetype: ${MULLE_OBJC_HTTP_FOUNDATION_NODETYPE:-tar}
+#
+export MULLE_OBJC_HTTP_FOUNDATION_NODETYPE="git"
+
+
+#
+# MulleObjCHTTPFoundation url: ${MULLE_OBJC_HTTP_FOUNDATION_URL:-https://github.com/MulleWeb/MulleObjCHTTPFoundation/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_OBJC_HTTP_FOUNDATION_URL="https://github.com/MulleWeb/MulleObjCHTTPFoundation.git"
+
+
+#
+# MulleObjCHTTPFoundation branch: ${MULLE_OBJC_HTTP_FOUNDATION_BRANCH}
+#
+export MULLE_OBJC_HTTP_FOUNDATION_BRANCH="prerelease"
+
+
