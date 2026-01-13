926e1ecf5dbcea16d785101bdadd16d9ec135583
Nat! <nat@mulle-kybernetik.com>
2026-01-13 22:51:14 +0100

    maintenance: update tooling and dependencies
    Version bumped to 0.18.8 in clib.json. Updated mulle-sde tooling versions in .mulle/share/sde/version. Added includes for mulle-core-all-load and mulle-rbtree-debug in test/include.h.

--- src/MulleObjCHTTPFoundation.h
+++ src/MulleObjCHTTPFoundation.h
@@ -7,7 +7,7 @@
 //
 #import "import.h"
 
-#define MULLE_OBJC_HTTP_FOUNDATION_VERSION  ((0UL << 20) | (18 << 8) | 8)
+#define MULLE_OBJC_HTTP_FOUNDATION_VERSION  ((0UL << 20) | (18 << 8) | 9)
 
 #import "_MulleObjCHTTPFoundation-export.h"
 
